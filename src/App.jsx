12cff2a90cc397137ab661a035c9ea15fcd41cb3
tsunodajapa <gabrieltsunoda@gmail.com>
2023-04-13 10:34:02 -0300

    feat: book provider

--- src/App.jsx
+++ src/App.jsx
@@ -14,3 +14,16 @@ export const App = () => {
 };
 
 // Rota => o caminho da sua página => /home, /register, /feed
+
+
+{/* 
+<A value={2}>
+  <B value={3}>
+    <C/>
+  </B>
+</A> 
+*/}
+
+// C consegue acessar 2 e 3
+// B consegue acessa numero 2
+// A não consegue acessar numero 3
